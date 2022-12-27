3f47368267f6dc4e8a9ce1d1fbd1f6690d0d63a8
Mainsun <121495306+Mainsun@users.noreply.github.com>
2022-12-27 13:25:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,35 +4,38 @@ import math
 import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+def find_prospects(industry, area):
+  # Set the search URL
+  search_url = f"https://www.google.com/search?q={industry}+companies+in+{area}"
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+  # Send the request and get the response
+  response = requests.get(search_url)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+  # Check the status code of the response
+  if response.status_code == 200:
+    # Extract the companies from the search results
+    companies = extract_companies_from_search_results(response.text)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+    # Return the companies as a JSON object
+    return { "companies": companies }
+  else:
+    # Return an error message if the request failed
+    return { "error": f"An error occurred: {response.status_code}" }
 
+def extract_companies_from_search_results(html):
+  # Use BeautifulSoup to parse the HTML
+  soup = BeautifulSoup(html, "html.parser")
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+  # Find all the h3 elements with the class "LC20lb"
+  h3_elements = soup.find_all("h3", class_="LC20lb")
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+  # Extract the company names from the h3 elements
+  companies = [element.text for element in h3_elements]
 
-    points_per_turn = total_points / num_turns
+  return companies
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+# Find prospects in the technology industry in San Francisco
+results = find_prospects("technology", "San Francisco")
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# Print the results
+print(results)
